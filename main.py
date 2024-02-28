3379d7e273445465ef58026d4f3904cce3b0bbf3
Michail S <mixan_45@mail.ru>
2024-02-28 10:50:28 +0300

    v1.0.1

--- main.py
+++ main.py
@@ -1,1 +1,24 @@
-#dev push
+from aiogram import Bot, Dispatcher, types
+import asyncio
+from aiogram.filters import CommandStart
+
+bot = Bot(token=token)
+dp = Dispatcher()
+
+
+@dp.message(CommandStart())
+async def start_cmd(message: types.Message):
+    await message.answer(text='Вы запустили бота')
+
+
+@dp.message()
+async def start_cmd(message: types.Message):
+    await message.answer(text=message.text)
+
+
+async def main():
+    await bot.delete_webhook(drop_pending_updates=True)
+    await dp.start_polling(bot)
+
+
+asyncio.run(main())
