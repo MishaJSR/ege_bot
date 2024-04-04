af6a36ed3255da551d92dfa179abae5a236bc296
Michail S <mixan_45@mail.ru>
2024-04-04 15:20:36 +0300

    v1.0.38 redis

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 import os
 import asyncio
 
-from aiogram import Bot, Dispatcher, types
+from aiogram import Bot, Dispatcher
 from aiogram.types import BotCommandScopeAllPrivateChats
+from aiogram.fsm.storage.redis import RedisStorage
 from dotenv import find_dotenv, load_dotenv
 
 load_dotenv(find_dotenv())
@@ -11,14 +12,24 @@ from handlers.user.user_main_router import user_private_router
 from handlers.admin.admin_main_router import admin_private_router
 from common.bot_cmd_list import private
 from middlewares.db import DataBaseSession
-from middlewares.admin import AdminMiddleware
 from database.engine import create_db, drop_db, session_marker
 
+
+def get_storage(redis):
+    url = f"redis://:{redis['password']}@{redis['host']}:{redis['port']}/{redis['db']}"
+    return RedisStorage.from_url(url)
+
+
+redis = {
+    'user': 'user',
+    'host': '127.0.0.1',
+    'port': 6379,
+    'db': 0,
+    'password': ''
+}
 ALLOWED_UPDATES = ['message, edited_message']
 bot = Bot(token=os.getenv('TOKEN'))
-
-dp = Dispatcher()
-#admin_private_router.message.outer_middleware(AdminMiddleware())
+dp = Dispatcher(storage=get_storage(redis))
 dp.include_routers(admin_private_router, user_private_router)
 
 
@@ -32,7 +43,6 @@ async def on_startup(bot):
 
 async def on_shutdown(bot):
     print('Bot end')
-
 
 
 async def main():
