5cf83c9aac3795cc6c9aec3fe220434b5c3fb5e5
misha <mixan_45@mail.ru>
2024-04-12 14:08:28 +0300

    2.0.13 postgres

--- database/models.py
+++ database/models.py
@@ -3,16 +3,15 @@ from sqlalchemy import String, Text, DateTime, func, Integer, Boolean
 
 
 class Base(DeclarativeBase):
-    created: Mapped[DateTime] = mapped_column(DateTime, default=func.now()),
     updated: Mapped[DateTime] = mapped_column(DateTime, default=func.now(), onupdate=func.now())
 
 
 class Task(Base):
     __tablename__ = 'task'
-    id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
-    exam: Mapped[str] = mapped_column(String(10), nullable=False)
+    id: Mapped[int] = mapped_column(Integer, primary_key=True, autoincrement=True)
+    exam: Mapped[str] = mapped_column(String(20), nullable=False)
     chapter: Mapped[str] = mapped_column(String(20), nullable=False)
-    under_chapter: Mapped[str] = mapped_column(String(20), nullable=False)
+    under_chapter: Mapped[str] = mapped_column(Text, nullable=False)
     description: Mapped[str] = mapped_column(Text, nullable=False)
     answer_mode: Mapped[str] = mapped_column(String(10), nullable=False)
     answers: Mapped[str] = mapped_column(Text, nullable=False)
