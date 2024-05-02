7c16f22f1539ec1a6c7dd5f91fe1c13b92045765
Michail S <mixan_45@mail.ru>
2024-05-02 13:17:03 +0300

    2.0.30

--- migrations/env.py
+++ migrations/env.py
@@ -1,29 +1,38 @@
 from logging.config import fileConfig
 
-from sqlalchemy import engine_from_config
+from sqlalchemy import engine_from_config, URL
 from sqlalchemy import pool
+from environs import Env
 
 from alembic import context
 
-# this is the Alembic Config object, which provides
-# access to the values within the .ini file in use.
+
 config = context.config
 
-# Interpret the config file for Python logging.
-# This line sets up loggers basically.
+
 if config.config_file_name is not None:
     fileConfig(config.config_file_name)
 
-# add your model's MetaData object here
-# for 'autogenerate' support
-# from myapp import mymodel
-# target_metadata = mymodel.Base.metadata
+env = Env()
+env.read_env('.env')
+
+print(env)
+
+url = URL.create(
+    drivername=f"postgresql+asyncpg",
+    host=env.str("DB_HOST"),
+    password=env.str("POSTGRES_PASSWORD"),
+    username=env.str("POSTGRES_USER"),
+    database=env.str("POSTGRES_DB"),
+    port=5432,
+).render_as_string(hide_password=False)
+
+config.set_main_option("sqlalchemy.url",
+                       url)
+
 target_metadata = None
 
-# other values from the config, defined by the needs of env.py,
-# can be acquired:
-# my_important_option = config.get_main_option("my_important_option")
-# ... etc.
+
 
 
 def run_migrations_offline() -> None:
