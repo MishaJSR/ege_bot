8de3d83a6402cdfea3a79e04780f5ec8f2a82605
Michail S <mixan_45@mail.ru>
2024-08-13 17:45:38 +0300

    start 1.0.6 update

--- database/repository.py
+++ database/repository.py
@@ -19,9 +19,9 @@ class AbstractRepository(ABC):
     async def get_all_by_fields(self, **kwargs) -> list[AlchemyDataObject]:
         raise NotImplementedError
 
-    @abstractmethod
-    async def update_fields(self, **kwargs) -> list[AlchemyDataObject]:
-        raise NotImplementedError
+    # @abstractmethod
+    # async def update_fields(self, **kwargs) -> list[AlchemyDataObject]:
+    #     raise NotImplementedError
 
 
 class SQLAlchemyRepository(AbstractRepository):
@@ -44,15 +44,15 @@ class SQLAlchemyRepository(AbstractRepository):
         res_values = [el._data for el in kwargs.get("result_query").fetchall()]
         return [AlchemyDataObject(kwargs.get("data"), value) for value in res_values]
 
-    async def update_one_field(self, **kwargs) -> AlchemyDataObject:
-        try:
-            data = kwargs.get("data")
-            values = kwargs.get("values")
-            async with async_session_maker() as session:
-                query = update(self.model).where(self.model.user_id == 548349299).values(user_id=1)
-                await session.execute(query)
-                await session.commit()
-            return True
-        except Exception as e:
-            print(e)
-            return False
+    # async def update_one_field(self, **kwargs) -> AlchemyDataObject:
+    #     try:
+    #         data = kwargs.get("data")
+    #         values = kwargs.get("values")
+    #         async with async_session_maker() as session:
+    #             query = update(self.model).where(self.model.user_id == 548349299).values(user_id=1)
+    #             await session.execute(query)
+    #             await session.commit()
+    #         return True
+    #     except Exception as e:
+    #         print(e)
+    #         return False
